680e0b97e70de8beaea1afd755020d669e0d4659
Tony Zhang <tozhang665@gmail.com>
2021-09-06 22:01:39 -0700

    added API calls and automatic update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // const findFood = require("./scripts/script")
+//https://api.edamam.com/api/food-database/v2/parser?app_id=81004352&app_key=4525ccc584ab8228a8038d8fddfa8b28&ingr=eggs%20and%20bacon&nutrition-type=cooking
 
 const draggables = document.querySelectorAll(".draggable")
 const containers = document.querySelectorAll(".container")
@@ -25,6 +26,7 @@ containers.forEach(container =>{
       container.insertBefore(draggable,afterElement.element);
       // container.appendChild(draggable);
     }
+    // getNutritionInfo();
   })
 })
 
@@ -44,18 +46,148 @@ function getDragAfterElement(container,y){
   },{offset: Number.NEGATIVE_INFINITY})
 
 }
-// function findFood(food){
-  // let url = "https://api.edamam.com/api/nutrition-data?app_id=4f9d03e6&app_key=dff8c743ccd80a3db6f55d96a39188a1&nutrition-type=logging&ingr="
-  //   url = url + food;
-  //   let nutrition = {};
-  //   fetch(url).then((res)=>res.json()).then((val)=>console.log(val.data));
-  //   // return nutrition;
-  // }
-  
-  
-let url ="https://api.edamam.com/api/nutrition-data?app_id=4f9d03e6&app_key=dff8c743ccd80a3db6f55d96a39188a1&nutrition-type=logging&ingr=chicken"
-fetch(url).then((response) => response.json()).then(data => console.log(data));
 
-// fetchText()
-// findFood("chicken")
-console.log("ITS WORKING");
+function getNutritionInfo(){
+  let rSide = document.getElementById("nutr-right-side")
+  let canv = document.getElementById("myCanvas");
+  canv.parentNode.removeChild(canv);
+
+  let newCanv = document.createElement("canvas")
+  newCanv.id = "myCanvas"
+  rSide.appendChild(newCanv);
+    let url2 = "https://api.edamam.com/api/food-database/v2/parser?app_id=81004352&app_key=4525ccc584ab8228a8038d8fddfa8b28&ingr=eggs%20and%20rice&nutrition-type=cooking"
+    // let url ="https://api.edamam.com/api/nutrition-data?app_id=4f9d03e6&app_key=dff8c743ccd80a3db6f55d96a39188a1&nutrition-type=logging&ingr=chicken"
+    fetch(url2)
+      .then((response) => response.json())
+      .then(data => {
+        let foodCont = document.getElementById("nutr-left-side")
+
+        let parsedData = data["parsed"]
+        let extracted =[]
+        let carbs = 0;
+        let fats = 0;
+        let protein = 0;
+        let calories = 0;
+
+
+        parsedData.forEach((ele)=>{
+          let arr = [];
+          // arr.push(ele["food"]["label"]);
+
+          let food = document.createElement("h3")
+          food.innerText = ele["food"]["label"];
+          // detectedFoods.push(ele["food"]["label"]);
+          foodCont.appendChild(food);
+
+          arr.push(ele["food"]["nutrients"])
+          extracted.push(arr)
+
+
+          carbs = carbs + ele["food"]["nutrients"]["CHOCDF"];
+          fats = fats + ele["food"]["nutrients"]["FAT"];
+          protein = protein + ele["food"]["nutrients"]["PROCNT"];
+          calories = calories + ele["food"]["nutrients"]["ENERC_KCAL"];
+          
+        })
+
+
+        const labels =[
+          'Carbohydrates',
+          'Fats',
+          'Proteins',
+          'Calories'
+        ];
+
+        const data2 ={
+          labels:labels,
+          datasets:[{
+            label:"Nutriton Facts",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [carbs,fats,protein,calories],
+          }]
+        };
+
+        const config={
+          type:'bar',
+          data:data2,
+          options:{}
+        };
+
+        var myChart = new Chart(
+          document.getElementById('myCanvas'),
+          config
+        );
+        console.log(extracted)
+    });
+}
+
+function grabFoods(){
+  var children = [].slice.call(document.getElementById('lSide').getElementsByTagName('*'),0);
+
+  var elements = new Array(children.length);
+  var arrayLength = children.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var name = children[i].getAttribute("id");   
+    elements[i]=name;
+  }
+
+  var terms = [];
+  elements.forEach((ele,idx)=>{
+    if(idx%2 ===1){
+      terms.push(ele)
+    }
+  })
+
+  // console.log(elements.join(" "))
+  // console.log(terms)
+
+  return terms.join("%20and%20");
+}
+
+function updateChart(myChart){
+  let url = "https://api.edamam.com/api/food-database/v2/parser?app_id=81004352&app_key=4525ccc584ab8228a8038d8fddfa8b28&ingr="
+  let terms = grabFoods();
+  url = url+ terms + "&nutrition-type=cooking"
+  fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        let foodCont = document.getElementById("nutr-left-side")
+        foodCont.innerHTML = "";
+        let parsedData = data["parsed"]
+        let extracted = []
+        let carbs = 0;
+        let fats = 0;
+        let protein = 0;
+        let calories = 0;
+
+
+        parsedData.forEach((ele) => {
+          let arr = [];
+          // arr.push(ele["food"]["label"]);
+
+          let food = document.createElement("h3")
+          food.innerText = ele["food"]["label"];
+          // detectedFoods.push(ele["food"]["label"]);
+          foodCont.appendChild(food);
+
+          arr.push(ele["food"]["nutrients"])
+          extracted.push(arr)
+
+
+          carbs = carbs + ele["food"]["nutrients"]["CHOCDF"];
+          fats = fats + ele["food"]["nutrients"]["FAT"];
+          protein = protein + ele["food"]["nutrients"]["PROCNT"];
+          calories = calories + ele["food"]["nutrients"]["ENERC_KCAL"];
+        })
+        console.log(myChart);
+        console.log(myChart["data"]["datasets"]["0"]["data"]);
+        myChart["data"]["datasets"]["0"]["data"] = [carbs, fats, protein, calories];
+        // console.log(myChart.data.datasets.data);
+        myChart.update();
+      })
+    
+}
+
+
+
